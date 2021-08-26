9c06e7d5427151db9bd3253da6b73a94924c1e62
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-26 19:55:56 +0530

    Adding the main class file

---
+++ MainT.java
@@ -0,0 +1,10 @@
+package com.tictactoe;
+
+public class MainT {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the game of Tic Tac Toe");
+		TicTacToeGame g = new TicTacToeGame();
+		g.createBoard();
+       
+}
+}
