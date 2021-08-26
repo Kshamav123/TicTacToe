6568f53558c066aa554bc39ed8b66542d63adebb
Kshamav123 <Kshamav123>
2021-08-26 22:10:49 +0530

    To choose X or 0

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,6 +2,9 @@ package com.tictactoe;
 
 public class TicTacToeGame {
 	private char[] board = new char[10];
+	private char userOption;
+	private char computerOption;
+
 
 	public void createBoard() {
 
@@ -10,4 +13,31 @@ public class TicTacToeGame {
 		}
 
 	}
+
+	/*
+	 * UseCase 2 - To allow a player to choose X or O
+	 * 
+	 * userOption is allows the user to choose if he wants to play as X and O.
+	 * 
+	 *  computerOption automatically allows it to choose the other team opposite to what user has selected
+	 */
+	public void chooseOption() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter if you want to choose X or O: ");
+		userOption = sc.next().charAt(0);
+		if (userOption == 'X' || userOption == 'x') {
+			userOption = 'X';
+			computerOption = 'O';
+			System.out.println("Player Team: " + userOption);
+			System.out.println("Computer Team: " + computerOption);
+		} else {
+			userOption = 'O';
+			computerOption = 'X';
+			System.out.println("Player Team: " + userOption);
+			System.out.println("Computer Team: " + computerOption);
+		}
+		sc.close();
+	}
 }
+
+
