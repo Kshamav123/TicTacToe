4b986a3191101b36d94ce447f515cd169451ea21
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-30 18:58:43 +0530

    Updating the main method

--- MainT.java
+++ MainT.java
@@ -1,15 +1,37 @@
 package com.tictactoe;
 
+import java.util.Scanner;
+
 public class MainT {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the game of Tic Tac Toe");
 		
 	TicTacToeGame ticTacToeGame = new TicTacToeGame();
-	ticTacToeGame.createBoard();
-	ticTacToeGame.chooseOption();
-	ticTacToeGame.showBoard();
-    ticTacToeGame.userIndexValue();
+//	ticTacToeGame.createBoard();
+//	ticTacToeGame.chooseOption();
+//	ticTacToeGame.showBoard();
+//    ticTacToeGame.userIndexValue();
+//    ticTacToeGame.userMove();
+//    ticTacToeGame.tossMethod();
+//    ticTacToeGame.statistics();
+//    ticTacToeGame.computerMove();
+//    ticTacToeGame.getWinner();
+//    ticTacToeGame.identifyWinner();
+    ticTacToeGame.gameFlow();
+  //Asking the user if wants to play another game - UC13
+  		System.out.println("Want to start a new game?(Y/N) ");
+  		Scanner sc = new Scanner(System.in);
+		char newGame = sc .next().charAt(0); 
+  		if(newGame=='Y' || newGame=='y')
+  		{
+  			int winnerStatus = 0;
+  			ticTacToeGame.gameFlow();
+  		}
+  		else 
+  			System.out.println("Thank you for playing the game");
+  		
 	}
 }
+
 
 	
