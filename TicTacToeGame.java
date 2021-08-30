359de511255cce7c0f9462f2b4ece531724041b4
Kshamav123 <kshamavidya1999@gmail.com>
2021-08-30 11:48:28 +0530

    Ability to choose desired location

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,14 +1,17 @@
 package com.tictactoe;
 
+
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	private char[] board = new char[10];
 	private char userOption;
 	private char computerOption;
-
+	Scanner sc = new Scanner(System.in);
 
 	public void createBoard() {
 
-		for (int i = 1; i <=10; i++) {
+		for (int i = 0; i <9 ; i++) {
 			board[i] = ' ';
 		}
 
@@ -19,7 +22,9 @@ public class TicTacToeGame {
 	 * 
 	 * userOption is allows the user to choose if he wants to play as X and O.
 	 * 
-	 *  computerOption automatically allows it to choose the other team opposite to what user has selected
+	 * @ computerOption automatically allows it to choose the other team opposite to
+	 * what user has selected. the 'if...else' condition automatically allocates
+	 * computer option
 	 */
 	public void chooseOption() {
 		Scanner sc = new Scanner(System.in);
@@ -36,19 +41,36 @@ public class TicTacToeGame {
 			System.out.println("Player Team: " + userOption);
 			System.out.println("Computer Team: " + computerOption);
 		}
-		sc.close();
+	
 	}
-	//Method to display the board
+
+//Method to display the board
 	public void showBoard()
 	{
-		System.out.println("\n------------- ");
 		for(int i = 1 ; i<10 ; i+=3)
 		{
-			System.out.println("| " + board[i] + " | " + board[i+1] + " | " + board[i+2] + " |");
+			System.out.println("| " + board[i] + " | " + board[i+1] + " | " + board[i+2] );
 			System.out.println("------------- ");
 		}
 		
 	}
+	/*
+	 * UseCase 4 - The user to make desired location in the board
+	 * In this method the user is asked to input as to where he wants to place his X or O
+	 * indexNumber is used for the user to enter his input number.
+	 * The 'if' condition checks for a valid index number.
+	 */
+	public void userIndexValue()
+	{
+		int indexNumber;
+		System.out.println("Enter an index number where you want to place your input: ");
+		indexNumber = sc.nextInt(); 
+		if(indexNumber < 1 || indexNumber > 9)
+		{
+			System.out.println("Please enter a valid index number from 1-9. ");
+		}
+	
+
+	}
+	
 }
-
-
